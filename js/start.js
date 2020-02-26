59f2359cdbada71329f8812da1ef97d806584564
Ilia Selieznov <ziziron21@gmail.com>
2020-02-27 00:05:15 +0200

    slider done

--- js/start.js
+++ js/start.js
@@ -10,7 +10,7 @@ function drawSliderCards() {
                 let imgPath = key['preview'][0];
                 let priceCard = key['price'];
                 let image = `<img src = " ${imgPath} " alt = " ${key['title']}" title = " ${key['title']}">`;
-                let price = `£${priceCard}`;
+                let price = `${priceCard}`;
                 cardItem.push({
                     title: key['title'],
                     price: price,
@@ -36,7 +36,7 @@ function drawSliderCards() {
                 let imgPath2 = key2['preview'][0];
                 let priceCard2 = key2['price'];
                 let image2 = `<img src = " ${imgPath2} " alt = " ${key2['title']}" title = " ${key2['title']}">`;
-                let price2 = `£${priceCard2}`;
+                let price2 = `${priceCard2}`;
                 cardItem.push({
                     title: key2['title'],
                     price: price2,
@@ -57,91 +57,100 @@ function drawSliderCards() {
     });
 }
 
+let slider = document.getElementById('slider__wrapper-left');
+let counterLeft = 0;
+let offset = 0;
+let slider2 = document.getElementById('slider__wrapper-right');
+
+let counterRight = 0;
+let offsetRight = 0;
+let btnRightTop = document.getElementById('slider__button-right-top');
+let btnRightDown = document.getElementById('slider__button-right-down');
+let btnLeftTop = document.getElementById('slider__button-left-top');
+let btnLeftDown = document.getElementById('slider__button-left-down');
+
+btnRightTop.addEventListener('click', function () {
+    if (offsetRight === 0) {
+        offsetRight += -450 * (window.bestOffer.right.length - 1);
+    } else {
+        offsetRight += 450;
+    }
+    slider2.style.top = offsetRight + 'px';
+    if (counterRight === 0) {
+        counterRight = window.bestOffer.right.length - 1;
+    } else if (counterRight <= window.bestOffer.right.length - 1) {
+        counterRight--;
+    }
+});
+btnRightDown.addEventListener('click', function () {
+    if (offsetRight > -450 * (window.bestOffer.right.length - 1)) {
+        offsetRight += -450;
+    } else if (offsetRight === -450 * (window.bestOffer.right.length - 1)) {
+        offsetRight = 0;
+    }
+    slider2.style.top = offsetRight + 'px';
+    if (counterRight < window.bestOffer.right.length - 1) {
+        counterRight++;
+    } else if (counterRight === window.bestOffer.right.length - 1) {
+        counterRight = 0;
+    }
+});
+btnLeftDown.addEventListener('click', function () {
+    if (offset > -450 * (window.bestOffer.left.length - 1)) {
+        offset += -450;
+    } else if (offset === -450 * (window.bestOffer.left.length - 1)) {
+        offset = 0;
+    }
+    slider.style.top = offset + 'px';
+    if (counterLeft < window.bestOffer.left.length - 1) {
+        counterLeft++;
+    } else if (counterLeft === window.bestOffer.left.length - 1) {
+        counterLeft = 0;
+    }
+});
+btnLeftTop.addEventListener('click', function () {
+    if (offset === 0) {
+        offset += -450 * (window.bestOffer.left.length - 1);
+    } else {
+        offset += 450;
+    }
+    slider.style.top = offset + 'px';
+    if (counterLeft === 0) {
+        counterLeft = window.bestOffer.left.length - 1;
+    } else if (counterLeft <= window.bestOffer.left.length - 1) {
+        counterLeft--;
+    }
+});
+
+btnLeftTop.addEventListener('click', () => {
+    let a = document.getElementById('notDiscountedPrice');
+    let b = document.getElementById('discountedPrice');
+    a.innerHTML = `£${+priceLeft[counterLeft].price + +priceRight[counterRight].price}`;
+    b.innerHTML = `£${(+priceLeft[counterLeft].price + +priceRight[counterRight].price) - window.bestOffer.discount}`;
+});
+btnRightDown.addEventListener('click', () => {
+    let a = document.getElementById('notDiscountedPrice');
+    let b = document.getElementById('discountedPrice');
+    a.innerHTML = `£${+priceLeft[counterLeft].price + +priceRight[counterRight].price}`;
+    b.innerHTML = `£${(+priceLeft[counterLeft].price + +priceRight[counterRight].price) - window.bestOffer.discount}`;
+});
+btnLeftDown.addEventListener('click', () => {
+    let a = document.getElementById('notDiscountedPrice');
+    let b = document.getElementById('discountedPrice');
+    a.innerHTML = `£${+priceLeft[counterLeft].price + +priceRight[counterRight].price}`;
+    b.innerHTML = `£${(+priceLeft[counterLeft].price + +priceRight[counterRight].price) - window.bestOffer.discount}`;
+});
+btnRightTop.addEventListener('click', () => {
+    let a = document.getElementById('notDiscountedPrice');
+    let b = document.getElementById('discountedPrice');
+    a.innerHTML = `£${+priceLeft[counterLeft].price + +priceRight[counterRight].price}`;
+    b.innerHTML = `£${(+priceLeft[counterLeft].price + +priceRight[counterRight].price) - window.bestOffer.discount}`;
+});
+
 drawSliderCards();
 
-function moveCardsLeft() {
-    document.getElementById('slider__button-left-top').onclick = leftTop;
-    document.getElementById('slider__button-left-down').onclick = leftDown;
-    let slider = document.getElementById('slider__wrapper-left');
 
-    // window.bestOffer.left.length
-    let counterLeft  = 0;
-    let offset = 0;
 
-    function leftDown() {
 
-        if (offset > -450 * (window.bestOffer.left.length - 1)) {
-            offset += -450;
-        } else if (offset === -450 * (window.bestOffer.left.length - 1)) {
-            offset = 0;
-        }
-        slider.style.top = offset + 'px';
-        if(counterLeft < window.bestOffer.left.length - 1){
-            counterLeft++;
-        } else if (counterLeft === window.bestOffer.left.length - 1){
-            counterLeft = 0;
-        }
-        console.log(window.bestOffer.left[counterLeft])
-    }
 
-    function leftTop() {
 
-        if (offset === 0) {
-            offset += -450 * (window.bestOffer.left.length - 1);
-        } else {
-            offset += 450;
-        }
-        slider.style.top = offset + 'px';
-        if(counterLeft === 0){
-            counterLeft = window.bestOffer.left.length - 1;
-        } else if (counterLeft <= window.bestOffer.left.length - 1){
-            counterLeft --;
-        }
-        console.log(window.bestOffer.left[counterLeft])
-    }
-
-}
-function moveCardsRight() {
-    document.getElementById('slider__button-right-top').onclick = rightTop;
-    document.getElementById('slider__button-right-down').onclick = rightDown;
-    let slider = document.getElementById('slider__wrapper-right');
-
-    // window.bestOffer.left.length
-    let counterRight  = 0;
-    let offsetRight = 0;
-
-    function rightDown() {
-
-        if (offsetRight > -450 * (window.bestOffer.right.length - 1)) {
-            offsetRight += -450;
-        } else if (offsetRight === -450 * (window.bestOffer.right.length - 1)) {
-            offsetRight = 0;
-        }
-        slider.style.top = offsetRight + 'px';
-        if(counterRight < window.bestOffer.right.length - 1){
-            counterRight++;
-        } else if (counterRight === window.bestOffer.right.length - 1){
-            counterRight = 0;
-        }
-        console.log(window.bestOffer.right[counterRight])
-    }
-
-    function rightTop() {
-
-        if (offsetRight === 0) {
-            offsetRight += -450 * (window.bestOffer.right.length - 1);
-        } else {
-            offsetRight += 450;
-        }
-        slider.style.top = offsetRight + 'px';
-        if(counterRight === 0){
-            counterRight = window.bestOffer.right.length - 1;
-        } else if (counterRight <= window.bestOffer.right.length - 1){
-            counterRight --;
-        }
-        console.log(window.bestOffer.right[counterRight])
-    }
-
-}
-moveCardsLeft();
-moveCardsRight();
