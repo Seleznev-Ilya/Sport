823af45bfe023a599f38c0560b6cf6156f5e3564
Ilia Selieznov <ziziron21@gmail.com>
2020-03-01 20:22:00 +0200

    filter table no line and mobil

--- js/catalog.js
+++ js/catalog.js
@@ -6,6 +6,8 @@ price.addEventListener('click', f);
 function f() {
     let inputPrice = document.querySelector('.price input[type=radio]:checked');
     if( inputPrice.alt !== 'Not selected' ){
+        inputPrice.previousElementSibling.classList.toggle( 'label--active');
+
         document.querySelector('.price .item__name').innerText = inputPrice.alt;
         let item = document.querySelector('.price .filter__item-section');
         item.style.backgroundColor = '#f7f7f7';
@@ -19,6 +21,9 @@ function f() {
         category.style.left = 20 + 'px';
         category.style.fontSize = 12 + 'px';
     } else {
+        if(inputPrice.alt === 'Not selected'){
+            inputPrice.previousElementSibling.classList.toggle( 'label--active-not');
+        }
         document.querySelector('.price .item__name').innerText = "";
         let category = document.querySelector('.price .item__category');
         category.style.top = 'calc(50% - 12px)';
@@ -35,6 +40,8 @@ size.addEventListener('click', f2);
 function f2() {
     let inputPrice = document.querySelector('.size input[type=radio]:checked');
     if( inputPrice.alt !== 'Not selected' ){
+        inputPrice.previousElementSibling.classList.toggle( 'label--active');
+
         document.querySelector('.size .item__name').innerText = inputPrice.alt;
         let item = document.querySelector('.size .filter__item-section');
         item.style.backgroundColor = '#f7f7f7';
@@ -48,6 +55,9 @@ function f2() {
         category.style.left = 20 + 'px';
         category.style.fontSize = 12 + 'px';
     } else {
+        if(inputPrice.alt === 'Not selected'){
+            inputPrice.previousElementSibling.classList.toggle( 'label--active-not');
+        }
         document.querySelector('.size .item__name').innerText = "";
         let category = document.querySelector('.size .item__category');
         category.style.top = 'calc(50% - 12px)';
@@ -64,6 +74,8 @@ brand.addEventListener('click', f3);
 function f3() {
     let inputPrice = document.querySelector('.brand input[type=radio]:checked');
     if( inputPrice.alt !== 'Not selected' ){
+        inputPrice.previousElementSibling.classList.toggle( 'label--active');
+
         document.querySelector('.brand .item__name').innerText = inputPrice.alt;
         let item = document.querySelector('.brand .filter__item-section');
         item.style.backgroundColor = '#f7f7f7';
@@ -77,6 +89,9 @@ function f3() {
         category.style.left = 20 + 'px';
         category.style.fontSize = 12 + 'px';
     } else {
+        if(inputPrice.alt === 'Not selected'){
+            inputPrice.previousElementSibling.classList.toggle( 'label--active-not');
+        }
         document.querySelector('.brand .item__name').innerText = "";
         let category = document.querySelector('.brand .item__category');
         category.style.top = 'calc(50% - 12px)';
@@ -93,6 +108,8 @@ color.addEventListener('click', f4);
 function f4() {
     let inputPrice = document.querySelector('.color input[type=radio]:checked');
     if( inputPrice.alt !== 'Not selected' ){
+        inputPrice.previousElementSibling.classList.toggle( 'label--active');
+
         document.querySelector('.color .item__name').innerText = inputPrice.alt;
         let item = document.querySelector('.color .filter__item-section');
         item.style.backgroundColor = '#f7f7f7';
@@ -106,6 +123,9 @@ function f4() {
         category.style.left = 20 + 'px';
         category.style.fontSize = 12 + 'px';
     } else {
+        if(inputPrice.alt === 'Not selected'){
+            inputPrice.previousElementSibling.classList.toggle( 'label--active-not');
+        }
         document.querySelector('.color .item__name').innerText = "";
         let category = document.querySelector('.color .item__category');
         category.style.top = 'calc(50% - 12px)';
@@ -122,6 +142,8 @@ product.addEventListener('click', f5);
 function f5() {
     let inputPrice = document.querySelector('.product input[type=radio]:checked');
     if( inputPrice.alt !== 'Not selected' ){
+        inputPrice.previousElementSibling.classList.toggle( 'label--active');
+
         document.querySelector('.product .item__name').innerText = inputPrice.alt;
         let item = document.querySelector('.product .filter__item-section');
         item.style.backgroundColor = '#f7f7f7';
@@ -135,6 +157,9 @@ function f5() {
         category.style.left = 20 + 'px';
         category.style.fontSize = 12 + 'px';
     } else {
+        if(inputPrice.alt === 'Not selected'){
+            inputPrice.previousElementSibling.classList.toggle( 'label--active-not');
+        }
         document.querySelector('.product .item__name').innerText = "";
         let category = document.querySelector('.product .item__category');
         category.style.top = 'calc(50% - 12px)';
@@ -149,7 +174,11 @@ fashion.addEventListener('click', f6);
 
 function f6() {
     let inputPrice = document.querySelector('.fashion input[type=radio]:checked');
+
+
     if( inputPrice.alt !== 'Not selected' ){
+
+        inputPrice.previousElementSibling.classList.toggle( 'label--active');
         document.querySelector('.fashion .item__name').innerText = inputPrice.alt;
         let item = document.querySelector('.fashion .filter__item-section');
         item.style.backgroundColor = '#f7f7f7';
@@ -163,6 +192,10 @@ function f6() {
         category.style.left = 20 + 'px';
         category.style.fontSize = 12 + 'px';
     } else {
+        if(inputPrice.alt === 'Not selected'){
+            inputPrice.previousElementSibling.classList.toggle( 'label--active-not');
+        }
+
         document.querySelector('.fashion .item__name').innerText = "";
         let category = document.querySelector('.fashion .item__category');
         category.style.top = 'calc(50% - 12px)';
